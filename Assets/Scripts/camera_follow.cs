8560c2f692c615bf3971e7fa8a39259bdc860cf3
Purplotl <NathanDonovanEmail@gmail.com>
2025-01-24 18:11:51 +0000

    Camera smoothing
    +Smooth camera movement

--- Assets/Scripts/camera_follow.cs
+++ Assets/Scripts/camera_follow.cs
@@ -7,6 +7,10 @@ public class camera_follow : MonoBehaviour
     [SerializeField] private float cameraMaxLeft;
     [SerializeField] private float cameraMaxTop;
     [SerializeField] private float cameraMaxBottom;
+    [SerializeField] private float panTime = 0.25f;
+
+    private Vector3 velocity = Vector3.zero;
+
     private Transform player;
     private void Awake()
     {
@@ -14,10 +18,10 @@ public class camera_follow : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Vector3 targetPos = new Vector3(player.position.x, player.position.y, -10f);
+        Vector3 pos = new Vector3(player.position.x, player.position.y, -10f);
 
-        CameraRestraintsX(targetPos);
-        CameraRestraintsY(targetPos);
+        CameraRestraintsX(pos);
+        CameraRestraintsY(pos);
     }
 
     private void CameraRestraintsX(Vector3 pos)
@@ -27,7 +31,8 @@ public class camera_follow : MonoBehaviour
         }
         else
         {
-            transform.position = new Vector3(pos.x, transform.position.y, transform.position.z);
+            Vector3 targetPos = new Vector3(pos.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, panTime);
         }
     }
 
@@ -38,7 +43,8 @@ public class camera_follow : MonoBehaviour
         }
         else
         {
-            transform.position = new Vector3(transform.position.x, pos.y, transform.position.z);
+            Vector3 targetPos = new Vector3(transform.position.x, pos.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, panTime);
         }
     }
 }
