271d560ec19803cf01a55ce261fd624c94975ae0
Purplotl <NathanDonovanEmail@gmail.com>
2025-01-25 14:50:27 +0000

    Animal vaiants
    +Adds different variations to animal health and speed
    ++Chickens, Sheep, Cows and Pigs all move at different speeds and have different clean values
    
    +When clean is at max they stop moving

--- Assets/Scripts/enemy_health.cs
+++ Assets/Scripts/enemy_health.cs
@@ -4,14 +4,22 @@ namespace Enemy.behaviour
 {
     public class enemy_health : MonoBehaviour
     {
+        [SerializeField] private float damageMultiplier;
         public void Damage(float damage)
         {
+            damage = damage * damageMultiplier;
             GetComponent<SpriteRenderer>().color = new Color(GetComponent<SpriteRenderer>().color.r + damage, GetComponent<SpriteRenderer>().color.g + damage, GetComponent<SpriteRenderer>().color.b + damage);
+            CleanCheck();
         }
 
-        private void Update()
+        private void CleanCheck()
         {
-            
+            if(GetComponent<SpriteRenderer>().color.r >= 0.74901961f)
+            {
+                GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
+                GetComponent<Collider2D>().enabled = false;
+                GetComponent<enemy_movement>().enabled = false;
+            }
         }
     }
 }
