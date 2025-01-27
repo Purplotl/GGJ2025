0def59ff014fe3b752127ee0f18238a23f2ec6e2
Purplotl <NathanDonovanEmail@gmail.com>
2025-01-27 00:57:53 +0000

    Bigger island
    +Island bigger
    
    +minor fixes

--- Assets/Scripts/camera_follow.cs
+++ Assets/Scripts/camera_follow.cs
@@ -1,11 +1,6 @@
 using UnityEngine;
 public class camera_follow : MonoBehaviour
 {
-
-    [SerializeField] private float cameraMaxRight;
-    [SerializeField] private float cameraMaxLeft;
-    [SerializeField] private float cameraMaxTop;
-    [SerializeField] private float cameraMaxBottom;
     [SerializeField] private float panTime = 0.25f;
 
     private Vector3 velocity = Vector3.zero;
@@ -18,32 +13,6 @@ public class camera_follow : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 pos = new Vector3(player.position.x, player.position.y, -10f);
-
-        CameraRestraintsX(pos);
-        CameraRestraintsY(pos);
-    }
-
-    private void CameraRestraintsX(Vector3 pos)
-    {
-        if (pos.x >= cameraMaxRight || pos.x <= cameraMaxLeft)
-        {
-        }
-        else
-        {
-            Vector3 targetPos = new Vector3(pos.x, transform.position.y, transform.position.z);
-            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, panTime);
-        }
-    }
-
-    private void CameraRestraintsY(Vector3 pos)
-    {
-        if (pos.y >= cameraMaxTop || pos.y <= cameraMaxBottom)
-        {
-        }
-        else
-        {
-            Vector3 targetPos = new Vector3(transform.position.x, pos.y, transform.position.z);
-            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, panTime);
-        }
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, panTime);
     }
 }
